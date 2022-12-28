570a3929a07e71f9741bd38363a079ae41192ac1
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-27 23:18:22 -0300

    :technologist: Adicionando página de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,64 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <>
-      <h1>Next.js</h1>
-    </>
+    <Flex 
+      w='100vw' 
+      h='100vh' 
+      align='center'
+      justify='center' 
+    >
+      <Flex 
+        as='form' 
+        width='100%'
+        maxWidth={360}
+        bg='gray.800'
+        boxShadow='dark-lg' 
+        rounded='md'
+        p='8'
+        flexDir='column'
+      >
+        <Stack spacing='4'>
+          <FormControl>
+            <FormLabel htmlFor='email'>E-mail</FormLabel>
+            <Input 
+              id='email'
+              type='email' 
+              name='email' 
+              focusBorderColor='purple.500'
+              bgColor='gray.900'
+              variant='filled'
+              size='lg'
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor='password'>Senha</FormLabel>
+            <Input 
+              id='password'
+              type='password' 
+              name='password' 
+              focusBorderColor='purple.500'
+              bgColor='gray.900'
+              variant='filled'
+              size='lg'
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button 
+          type='submit' 
+          mt='6' 
+          colorScheme='purple'
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
