a10a336f9fa80b5b3aee679dd42a5cb8725d80b2
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-12-28 01:45:41 -0300

    Criando component Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl, Image } from '@chakra-ui/react'
+import { Flex, Button, Stack, Image } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex 
       w='100vw' 
@@ -8,67 +10,46 @@ export default function Home() {
       align='center'
       justify='center' 
     >
-      <Flex 
-        as='form' 
-        width='100%'
-        maxWidth={460}
-        bg='gray.800'
-        boxShadow='4px 4px 8px 4px #4B4D63' 
-        rounded='md'
-        p='8'
-        flexDir='column'
+      <motion.div 
+        className='box'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.3 }}
       >
-        <Stack spacing='4'>
-          <div>
-            <Image
-              src='https://bit.ly/dan-abramov'
-              alt='Dan Abramov'
-              title='Dan Abramov'
-              borderRadius='full'
-              boxSize='50%'
-              margin='auto'
-            />
-          </div>
-          <FormControl>
-            <FormLabel htmlFor='email'>E-mail</FormLabel>
-            <Input 
-              id='email'
-              type='email' 
-              name='email' 
-              focusBorderColor='purple.500'
-              bgColor='gray.900'
-              variant='filled'
-              size='lg'
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor='password'>Senha</FormLabel>
-            <Input 
-              id='password'
-              type='password' 
-              name='password' 
-              focusBorderColor='purple.500'
-              bgColor='gray.900'
-              variant='filled'
-              size='lg'
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-            />
-          </FormControl>
-        </Stack>
-
-        <Button 
-          type='submit' 
-          mt='6' 
-          colorScheme='purple'
+        <Flex 
+          as='form' 
+          width='100%'
+          maxWidth={460}
+          bg='gray.800'
+          boxShadow='4px 4px 8px 4px #4B4D63' 
+          rounded='md'
+          p='8'
+          flexDir='column'
         >
-          Entrar
-        </Button>
-      </Flex>
+          <Stack spacing='4'>
+            <div>
+              <Image
+                src='https://bit.ly/dan-abramov'
+                alt='Dan Abramov'
+                title='Dan Abramov'
+                borderRadius='full'
+                boxSize='50%'
+                margin='auto'
+              />
+            </div>
+            <Input type='email' name='email' label='E-mail' />
+            <Input type='password' name='password' label='Senha' />
+          </Stack>
+
+          <Button 
+            type='submit' 
+            mt='6' 
+            colorScheme='purple'
+          >
+            Entrar
+          </Button>
+        </Flex>
+      </motion.div>
     </Flex>
   )
 }
