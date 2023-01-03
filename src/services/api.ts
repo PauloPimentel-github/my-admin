28e1c9a2b46ab201e2e3c3a655fd76dd616bb273
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-01-03 20:33:35 -0300

    Ajustando configuração de cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS, DELETE',
+        'Access-Control-Allow-Origin': '*'
+    }
 })
