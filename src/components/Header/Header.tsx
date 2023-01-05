6871cb7042e6c55e0244cfa6d4e6e396abcef203
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-01-05 20:57:36 -0300

    Finalizando componente Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Box, Text, Input, Icon, HStack, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -19,7 +19,7 @@ export function Header() {
                 letterSpacing='tight'
                 w='64'
             >
-                dashgo
+                myAdmin
                 <Text as='span' color='yellow.500' ml='1'>.</Text>
             </Text>
 
@@ -47,6 +47,35 @@ export function Header() {
                 />
 
                 <Icon as={RiSearchLine} fontSize='20' />
+            </Flex>
+
+            <Flex
+               align='center'
+               ml='auto' 
+            >
+                <HStack 
+                    spacing='8'
+                    mx='8'
+                    pr='8'
+                    py='1'
+                    color='gray.300'
+                    borderRightWidth={1}
+                    borderColor='gray.700'
+                >
+                    <Icon as={RiNotificationLine} fontSize='20' />
+                    <Icon as={RiUserAddLine} fontSize='20' />
+                </HStack>
+
+                <Flex align='center'>
+                    <Box mr='4' textAlign='right'>
+                        <Text>Paulo Pimentel</Text>
+                        <Text color='gray.300' fontSize='small'>
+                            paulo.h.g.pimentel@gmail.com
+                        </Text>
+                    </Box>
+
+                    <Avatar size='md' name='Paulo Pimentel' src='https://avatars.githubusercontent.com/u/25647587?v=4' />
+                </Flex>
             </Flex>
         </Flex>
     )
