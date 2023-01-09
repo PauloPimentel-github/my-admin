131a710e780f919ce4296671e6a65787a9f756bd
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2023-01-09 19:02:32 -0300

    Sidebar responsiva

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,17 @@ import { ChakraBaseProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 import { AuthProvider } from '../contexts/AuthContext'
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    
-      <ChakraBaseProvider theme={theme}>
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-      </ChakraBaseProvider>
+      <SidebarDrawerProvider>
+        <ChakraBaseProvider theme={theme}>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </ChakraBaseProvider>
+      </SidebarDrawerProvider>
   )
 }
 
